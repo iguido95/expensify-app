387a64298b2b8229002dabb28a984ca7615156b1
Guido Teunissen <iguido@me.com>
2019-07-02 12:01:28 +0200

    production ready for css

--- src/app.js
+++ src/app.js
@@ -10,29 +10,10 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-
-store.dispatch(addExpense({
-    description: "Water bill",
-    amount: 15000,
-    createdAt: 6
-}));
-
-store.dispatch(addExpense({
-    description: "Gas bill",
-    amount: 200000,
-    createdAt: 5
-}));
-
-store.dispatch(addExpense({
-    description: "Electricity bill",
-    amount: 210000,
-    createdAt: 8
-}));
-
-
-
 const expenses = store.getState().expenses;
 const filters = store.getState().filters;
+
+console.log('test');
 
 const jsx = (
     <Provider store={store}>
