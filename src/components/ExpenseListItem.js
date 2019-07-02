e1181e86d4fae433259ec18e8ff4ffa9d8cbeee7
Guido Teunissen <iguido@me.com>
2019-07-02 16:53:31 +0200

    currency format in expenselistitem

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
 
 const ExpenseListItem = ({expense, dispatch}) => (
-    <li><Link to={`edit/${expense.id}`}>{ expense.description }</Link> - { expense.amount/100 } ({expense.createdAt})</li>
+    <li>
+        <Link to={`edit/${expense.id}`}>
+        { expense.description }
+    </Link> 
+        - { numeral(expense.amount/100).format('€0.0,00') } 
+        ({moment(expense.createdAt).locale('nl').format('DD-MMMM-YYYY')})
+    </li>
 );
 
 
